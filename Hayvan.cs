f0e25933f44b2dcc5f48593c6635d36bd56644f3
Azra <yukselazrael@gmail.com>
2024-01-14 22:02:40 +0300

    Added Project

---
+++ Hayvan.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadingExample
+{
+    internal class Hayvan
+    {
+        public virtual void Ses()
+            //Kalııtm ile giden classtaki metota aşırı yükleme (override) yapar
+        {
+            Console.WriteLine("Sessiz bir hayvan.");
+        }
+
+    }
+
+    class Dog:Hayvan
+    {
+        public override void Ses() 
+            //Overriding (Kalıtım ile gelen metodu geçersiz kılar)
+        {
+            Console.WriteLine("Hav Hav");
+        }
+    }
+
+    
+}
---
+++ Math.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadingExample
+{
+    internal class Math
+    {
+        public int toplama(int x , int y)
+        {
+            return x + y;
+        }
+
+        public double toplama (double x, double y) //overloading
+        {
+            return x + y;
+        }
+
+
+    }
+}
